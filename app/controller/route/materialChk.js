c06ca83b19ae0e44525200f9689eff94056ddfb0
aksatyam <ashishsatyam.kmr@gmail.com>
2018-01-04 20:59:42 +0530

    material chk roter updated

--- app/controller/route/materialChk.js
+++ app/controller/route/materialChk.js
@@ -2,7 +2,7 @@
 let router= require('express').Router();
 const materialChkFunc = require('../process/materialChk');
 router.get('/test',materialChkFunc.test);
-router.get('/save', materialChkFunc.saveProduct);
+router.post('/save', materialChkFunc.saveProduct);
 router.post('/editProduct/:id', materialChkFunc.editProduct);
 router.get('/getAllProduct/:id', materialChkFunc.getAllProduct);
 router.get('/getOneProduct/:id', materialChkFunc.getOneProduct);
