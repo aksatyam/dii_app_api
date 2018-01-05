9494c1581c5ae2ade74080204a3aff5eee885170
aksatyam <ashishsatyam.kmr@gmail.com>
2018-01-05 09:01:37 +0530

    materialChk api upadted

--- app/controller/process/materialChk.js
+++ app/controller/process/materialChk.js
@@ -162,7 +162,7 @@ module.exports={
     },
     getOneProduct: async(req, res)=>{
         try{
-            let product=await MaterialChk.findOne({_id:req.params.id}).populate('indu_id material0 material1 material2 material3 material4 material5 material6 material7 material8 material9 material10 material11 material12 material13 material14 material15 material16 material17 material18 material19 material20 material21 material22 material23 material24');
+            let product=await MaterialChk.findOne({_id:req.params.id}).populate('indu_id product_id material0 material1 material2 material3 material4 material5 material6 material7 material8 material9 material10 material11 material12 material13 material14 material15 material16 material17 material18 material19 material20 material21 material22 material23 material24');
             if(!product)
             throw validation.errorFormat('Not Found','No Data Available for Industry',404);
             res.status(200).send({msg:'All Product Data',data:product});
