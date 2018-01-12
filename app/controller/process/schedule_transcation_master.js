7706381ca57c0770f50dba788f5bb6fe525fb0e2
aksatyam <ashishsatyam.kmr@gmail.com>
2018-01-12 11:53:25 +0530

    api updated

--- app/controller/process/schedule_transcation_master.js
+++ app/controller/process/schedule_transcation_master.js
@@ -6,5 +6,49 @@ const validation = require('../../helper/validation');
 module.exports={
     test(req,res){
         res.status(200).send({message: 'done'});
+    },
+    saveScheduleTrans: async(req,res)=>{
+
+    },
+    editScheduleTransaction: async(req, res)=>{
+
+    },
+    getAllScheduleTransaction: async(req,res)=>{
+        try{
+            let scheduleTrans=await ScheduleTranscation.find({indu_id:req.params.id}).populate('indu_id product_id');
+            if(!scheduleTrans){
+                throw validation.errorFormat('Not Found','No Data Available for Industry',404);
+            }
+            res.status(200).send({msg:'All Schedule Transaction Data',data:scheduleTrans});
+        }
+        catch(err){
+            let error;
+            if(!err.code || !err.status || !err.message) {
+                error = validation.errorFormat('internal_error', 'Internal server error', 500);
+            }
+            else{
+                error = err;
+            }
+            res.status(error.status).send({code: error.code, message: error.message});
+        }
+    },
+    getOneScheduleTransaction: async(req,res)=>{
+        try{
+            let scheduleTrans=await ScheduleTranscation.find({_id:req.params.id}).populate('indu_id product_id');
+            if(!scheduleTrans){
+                throw validation.errorFormat('Not Found','No Data Available for Industry',404);
+            }
+            res.status(200).send({msg:'All Schedule Transaction Data',data:scheduleTrans});
+        }
+        catch(err){
+            let error;
+            if(!err.code || !err.status || !err.message) {
+                error = validation.errorFormat('internal_error', 'Internal server error', 500);
+            }
+            else{
+                error = err;
+            }
+            res.status(error.status).send({code: error.code, message: error.message});
+        }
     }
 }
--- app/controller/route/schedule_transcation_master.js
+++ app/controller/route/schedule_transcation_master.js
@@ -2,4 +2,8 @@
 let router= require('express').Router();
 const scheduleTransFunc= require('../process/schedule_transcation_master');
 router.get('/test',scheduleTransFunc.test);
+router.post('/save',scheduleTransFunc.saveScheduleTrans);
+router.post('/edit',scheduleTransFunc.editScheduleTransaction);
+router.get('/getAll/:id',scheduleTransFunc.getAllScheduleTransaction);
+router.get('getOne/:id',scheduleTransFunc.getOneScheduleTransaction);
 module.exports = router;
