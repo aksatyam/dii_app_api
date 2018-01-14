7e8e10e2c961092201ebd643abee9cb2c4f176bf
aksatyam <ashishsatyam.kmr@gmail.com>
2018-01-14 12:58:01 +0530

    schedule trans save api updated

--- app/controller/process/schedule_transcation_master.js
+++ app/controller/process/schedule_transcation_master.js
@@ -8,7 +8,31 @@ module.exports={
         res.status(200).send({message: 'done'});
     },
     saveScheduleTrans: async(req,res)=>{
-        res.status(200).send({msg:'All Schedule Transaction Data',data:req});
+        try{
+            if(!req.body)
+            throw validation.errorFormat('empty_field', 'Data not present', 400);
+            let scheduleTrans=await ScheduleTranscation.findOne({ $and:[{indu_id:req.body.indu_id},{product_id:req.body.product_id}]});
+            if(scheduleTrans)
+            throw validation.errorFormat('duplicate', 'Product already Exist', 409);
+            let scheduleTransData=new ScheduleTranscation();
+            scheduleTransData.indu_id = req.body.indu_id;
+            scheduleTransData.product_id = req.body.product_id;
+            scheduleTransData.quantity = req.body.quantity;
+            scheduleTransData.fromDate = req.body.fromDate;
+            scheduleTransData.toDate = req.body.toDate;
+            await scheduleTransData.save();
+            res.status(200).send({msg: 'done', data: scheduleTransData});
+        }
+        catch(err){
+            let error;
+            if(!err.code || !err.status || !err.message) {
+                error = validation.errorFormat('internal_error', 'Internal server error', 500);
+            }
+            else{
+                error = err;
+            }
+            res.status(error.status).send({code: error.code, message: error.message});
+        }
     },
     editScheduleTransaction: async(req, res)=>{
 
