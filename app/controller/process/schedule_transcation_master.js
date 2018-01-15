0bc18db2611cc5a476e5f6c1433fa82f285224e4
aksatyam <ashishsatyam.kmr@gmail.com>
2018-01-15 21:26:12 +0530

    schedule transcation updated

--- app/controller/process/schedule_transcation_master.js
+++ app/controller/process/schedule_transcation_master.js
@@ -1,6 +1,7 @@
 'use strict'
 const mongoose = require('mongoose');
 const ScheduleTranscation = mongoose.model('Schedule_transaction_master');
+const materialChk = mongoose.model('MaterialChk');
 const Promise = require('bluebird');
 const validation = require('../../helper/validation');
 module.exports={
@@ -21,6 +22,7 @@ module.exports={
             scheduleTransData.fromDate = req.body.fromDate;
             scheduleTransData.toDate = req.body.toDate;
             await scheduleTransData.save();
+            // let trans=await materialChk.findOne({ $and:[{indu_id:req.body.indu_id},{product_id:req.body.product_id}]});
             res.status(200).send({msg: 'done', data: scheduleTransData});
         }
         catch(err){
