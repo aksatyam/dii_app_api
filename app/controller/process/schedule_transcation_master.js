7e9fce9c251284c39c60dc1bd744aa1941ae7cc8
aksatyam <ashishsatyam.kmr@gmail.com>
2018-01-17 22:28:29 +0530

    schedule transaction updated

--- app/controller/process/schedule_transcation_master.js
+++ app/controller/process/schedule_transcation_master.js
@@ -2,6 +2,7 @@
 const mongoose = require('mongoose');
 const ScheduleTranscation = mongoose.model('Schedule_transaction_master');
 const materialChk = mongoose.model('MaterialChk');
+const Material_available = mongoose.model('Material_available_master');
 const Promise = require('bluebird');
 const validation = require('../../helper/validation');
 module.exports={
@@ -11,10 +12,28 @@ module.exports={
     saveScheduleTrans: async(req,res)=>{
         try{
             if(!req.body)
-            throw validation.errorFormat('empty_field', 'Data not present', 400);
+                throw validation.errorFormat('empty_field', 'Data not present', 400);
             let scheduleTrans=await ScheduleTranscation.findOne({ $and:[{indu_id:req.body.indu_id},{product_id:req.body.product_id}]});
             if(scheduleTrans)
-            throw validation.errorFormat('duplicate', 'Product already Exist', 409);
+                throw validation.errorFormat('duplicate', 'Product already Exist', 409);
+            let product = await materialChk.findOne({product_id: req.body.product_id}).populate('materials.material_id');
+            let checkMaterialAvailable = product.materials.filter(d=> typeof d.material_id != 'object');
+            if(checkMaterialAvailable.length)
+                throw validation.errorFormat('row_material_absent', 'Some material not present', 409);
+            let metarilList = product.materials.map(d=> {return {_id: d.material_id._id, quantity: d.quantity * req.body.quantity}});
+            let checkArray = metarilList.map(d=> d._id);
+            let materialAvailData = await Material_available.find({material_master_id: {$in: checkArray}, updated_quantity: {$gt: 0} })
+            if(materialAvailData.length < checkArray.length)
+                throw validation.errorFormat('data_absent', 'some material is not available', 409);
+            metarilList = metarilList.filter(d=>{
+                let mat_id= materialAvailData.find(item=> item.material_master_id.toString()== d._id.toString());
+                if(d.quantity <= mat_id.updated_quantity)
+                    return d;
+            });
+            if(metarilList.length != materialAvailData.length)
+                throw validation.errorFormat('row_material_not_found', 'some material is not available', 409);
+            
+
             let scheduleTransData=new ScheduleTranscation();
             scheduleTransData.indu_id = req.body.indu_id;
             scheduleTransData.product_id = req.body.product_id;
@@ -22,10 +41,16 @@ module.exports={
             scheduleTransData.fromDate = req.body.fromDate;
             scheduleTransData.toDate = req.body.toDate;
             await scheduleTransData.save();
-            // let trans=await materialChk.findOne({ $and:[{indu_id:req.body.indu_id},{product_id:req.body.product_id}]});
-            res.status(200).send({msg: 'done', data: scheduleTransData});
+            res.status(200).send({msg:'done',data:scheduleTransData});
+            
+            for(let item of metarilList){
+                let material = await Material_available.findOne({material_master_id: item._id});
+                material.updated_quantity = material.updated_quantity - item.quantity;
+                material.save();
+            }
         }
         catch(err){
+            console.log(err);
             let error;
             if(!err.code || !err.status || !err.message) {
                 error = validation.errorFormat('internal_error', 'Internal server error', 500);
