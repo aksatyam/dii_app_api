fe56bf4b69a4a45783c0c94c17f61cd22b81991e
aksatyam <ashishsatyam.kmr@gmail.com>
2018-01-24 15:32:06 +0530

    materila master api updated

--- app/controller/route/material_master.js
+++ app/controller/route/material_master.js
@@ -3,7 +3,7 @@ let router= require('express').Router();
 const materialFunc = require('../process/material_master');
 router.get('/test',materialFunc.test);
 router.post('/save', materialFunc.saveMaterial);
-router.post('/editOne/:id', materialFunc.editMaterialOne);
+router.post('/editOne', materialFunc.editMaterialOne);
 router.get('/getAll/:id', materialFunc.getAllMaterial);
 router.get('/getOne/:id', materialFunc.getOneMaterial);
 module.exports = router;
